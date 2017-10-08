739b6b60dcf47eacda3d542f2a9746cd76ce5c1d
YuriiTrofimenko <tyaa@ukr.net>
2017-10-08 23:31:54 +0300

    include webview and jsoup

--- ticketbookeremulator/src/main/java/org/tyaa/ticketbookeremulator/WebActivity.java
+++ ticketbookeremulator/src/main/java/org/tyaa/ticketbookeremulator/WebActivity.java
@@ -1,24 +1,107 @@
 package org.tyaa.ticketbookeremulator;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.tyaa.ticketbookeremulator.impl.TicketBooker;
+
+import java.io.IOException;
 
 public class WebActivity extends AppCompatActivity {
 
-    WebView mWebView;
+    private WebView mWebView;
+    private final String BASE_URL = "https://www.onetwotrip.com/ru/poezda/";
+    private AppCompatActivity mSelf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        mSelf = this;
         setContentView(R.layout.activity_web);
         mWebView = (WebView) findViewById (R.id.webView);
         mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.loadUrl("https://www.onetwotrip.com/ru/poezda/");
+
+        /*Thread downloadThread = new Thread() {
+            public void run() {
+
+                Connection.Response res = null;
+                try {
+                    res =
+                        Jsoup.connect(BASE_URL)
+                                .method(Connection.Method.GET)
+                                .execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //Map<String, String> cookies = res.cookies();
+                if (res == null){
+                    Log.e("error", "Http request error");
+                }
+                Document doc = null;
+                try {
+                    doc = res.parse();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (doc == null) {
+                    Log.e("error", "Wep page parsing error");
+                } else {
+                    final Document docCopy = doc;
+                    // post a new Runnable from a Handler in order to run the WebView loading code from the UI thread
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //mWebView.loadData(element.html(), "text/html", "UTF-8");
+                            mWebView.loadDataWithBaseURL(BASE_URL, docCopy.html(), "text/html", "UTF-8", null);
+                        }
+                    });
+                }
+            }
+        };
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(getApplicationContext(), description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        downloadThread.start();*/
+        //TODO apply some compat method instead this for sdkVersion < 17
+        mWebView.addJavascriptInterface(new MyJavaScriptInterface(), "HtmlHandler");
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                mWebView.loadUrl("javascript:window.HtmlHandler.handleHtml" +
+                        "('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');");
+            }
+        });
+        mWebView.loadUrl(BASE_URL);
+
         TicketBooker.setBooked(true);
-        Toast.makeText(this, String.valueOf(TicketBooker.isBooked()), Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, String.valueOf(TicketBooker.isBooked()), Toast.LENGTH_LONG).show();
+    }
+
+    private class MyJavaScriptInterface {
+        @JavascriptInterface
+        public void handleHtml(String html) {
+            // Use jsoup on this String here to search for your content.
+            Document doc = Jsoup.parse(html);
+
+            // Now you can, for example, retrieve a div with id="username" here
+            Element submitButton = doc.select("button[type=submit]").first();
+            Toast.makeText(mSelf, submitButton.text(), Toast.LENGTH_LONG).show();
+        }
     }
 }
